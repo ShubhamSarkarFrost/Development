6de6c69be7e9d4c222ddd8b8b4b1979d26568bf5
Shubham Sarkar <ronniecse8@gmail.com>
2022-07-17 11:47:20 +0530

    WEB_Development_JavaScript_Basics_1(Complete)_11:46_17_07_2022_IST

---
+++ Javascript/JavaScript/Primitive_DataType.js
@@ -0,0 +1,30 @@
+// alert("Hello World")
+//Positive Integer
+const integer = "89"
+console.log(integer)
+
+const floater = "132.89"
+console.log(floater)
+
+//Negative Integer
+const neginteger = "-89"
+console.log(neginteger)
+
+// Follows PEMDAS Rule
+const exp = (2*(3.5+2)/2)
+console.log(exp)
+
+//carried to Function
+const carriedto = 5 ** 2;
+console.log(carriedto)
+
+//Not a Number - NaN
+let nanone = 0/0;
+console.log(nanone)
+
+let nantwo = nanone + 1;
+console.log(nantwo)
+console.log(typeof(nantwo)) // Though it is considered as a Number , it is not a Number
+
+
+
---
+++ Javascript/JavaScript/Variables.js
@@ -0,0 +1,30 @@
+// check the link with a Print Statement
+// alert("Hi , Good Morning !!!")
+
+// Three types of Javascript Variable - var , let & Const
+// Let
+let year = 1985;
+console.log(year);
+
+//var
+var yeartwo = 1774;
+console.log(yeartwo)
+
+//const
+const yearthree = 1875;
+console.log(yearthree)
+
+//update variables
+let year = 1665;
+console.log(year)
+
+var yeartwo = 1885;
+console.log(yeartwo)
+
+//assign boolean value
+let isLoggeedOut = true;
+let gameOver = false;
+console.log(isLoggeedOut)
+console.log(gameOver)
+
+
